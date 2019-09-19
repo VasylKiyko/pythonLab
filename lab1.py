e857c1e2889a4fc7c62ebf40e5a5e55523aa7ca5
Vasyl Kiyko <vvvv@aaa.ddd>
2019-09-19 20:23:41 +0300

    Add new part

---
+++ lab1.py
@@ -0,0 +1,24 @@
+import random
+
+
+def fill_list(size):
+    List = []
+    i = 0
+    while i < size:
+        List.append(random.randint(0, 100))
+        i = i + 1
+    return List
+
+
+def list_sum(List):
+    sum = 0
+    for x in List:
+        sum = sum + x
+    return sum
+
+
+number = input("Enter size of list: ")
+List = fill_list(int(number))
+Sum = list_sum(List)
+avg = Sum / float(number)
+print("Sum of your list`s elements is " + str(Sum) + " and it`s average is " + str(avg))
