7d1df8687ca5737a1e5da02910fac9747cbaa6a8
Vasyl Kiyko <vasylkiyko1@gmail.com>
2019-10-24 22:21:09 +0300

    Add lab with SOLID

---
+++ lab5.py
@@ -0,0 +1,94 @@
+"""SOLID
+    S - single responsibilities
+    O - open / closed
+    L - Liskov
+    I - interface segregation
+    D - dependancy inversion
+    Тут я відтворив принцип S так : у мене є 3 класи, кожен з яких має
+    розподілені умови: 1 створює фігуру, 2 малює її, 3 проводить геометричні обрахунки.
+    Принцип О - тут він відображений наприклад у тому, що для обрахунку площі фігури, нам
+    не потрібно перевіряти тип фігури і під неї писати формулу обрахунку. Тут кожен клас фігури
+    сам відповідає за свою формулу.
+    Принцип L - пов'язаний з принципом O, ми можемо доступатись до функцій класу фігура через класи
+    Трикутник та Сфера.
+    Принцип I - класу Трикутник не потрібно реалізовувати функцію пошуку об'єму.
+    Принцип D - усі класи, і Трикутник , і Сфера залежать від абстракцій а
+    не від конкретних реалзіацій
+    """
+
+
+class Figure:
+    def getPosition(self):
+        pass
+
+    def setPosition(self, *args):
+        pass
+
+
+class Figure2DGeometry:
+    def calcS(self):
+        pass
+
+    def calcP(self):
+        pass
+
+
+class Figure3DGeometry:
+    def calcV(self):
+        pass
+
+
+class DrawFigure:
+    def draw(self):
+        pass
+
+
+class Triangle:
+    A = B = C = (0, 0)
+
+    def setPosition(self, a, b, c):
+        self.A = a
+        self.B = b
+        self.C = c
+
+    def getPosition(self):
+        print("Three points -- {0} , {1}, {2}".format(self.A, self.B, self.C))
+
+    def calcS(self):
+        pass
+
+    def caclP(self):
+        pass
+
+    def draw(self):
+        print("I`m a triangle!")
+
+
+class Sphere:
+    center = (0, 0)
+    radius = 0
+
+    def setPosition(self, c, r):
+        self.center = c
+        self.radius = r
+
+    def getPosition(self):
+        print("Center -- {0} and radius -- {1}".format(self.center, self.radius))
+
+    def calcV(self):
+        pass
+
+    def draw(self):
+        print("I`m a circle!")
+
+
+if __name__ == '__main__':
+    fig1 = Triangle()
+    fig2 = Triangle()
+    fig3 = Sphere()
+    fig1.setPosition((0, 0), (3, 5), (3, 7))
+    fig2.setPosition((0, -1), (7, 5), (3, 4))
+    fig3.setPosition((0, 0), 4)
+    lst = [fig1, fig2, fig3]
+    for i in lst:
+        i.draw()
